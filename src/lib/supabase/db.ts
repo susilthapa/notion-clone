8231c9f6e48a3213a9a3c546f997e3de04d687f5
Susil Thapa <thapasusil53@gmail.com>
2023-11-16 17:05:16 +0545

    Migration done

--- src/lib/supabase/db.ts
+++ src/lib/supabase/db.ts
@@ -1,24 +1,25 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as dotenv from "dotenv";
+import * as schema from "../../../migrations/schema";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
-import * as schema from "@/migrations/schema";
 
 dotenv.config({ path: ".env" });
 
 if (!process.env.DATABASE_URL) {
-  console.log("Can not find database url");
+  console.log("🔴 no database URL");
 }
 
 const client = postgres(process.env.DATABASE_URL as string, { max: 1 });
 const db = drizzle(client, { schema });
+
 const migrateDb = async () => {
   try {
-    console.log("Migrating client");
+    console.log("🟠 Migrating client");
     await migrate(db, { migrationsFolder: "migrations" });
-    console.log("Successfully migrated");
+    console.log("🟢 Successfully Migrated");
   } catch (error) {
-    console.log("Failed to migrate");
+    console.log("🔴 Error Migrating client", error);
   }
 };
 migrateDb();
